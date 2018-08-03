3318519ae4d7b955fcc9a31fd1561468998b6578
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-03 14:28:15 -0400

    Finish phase 3

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_accessor :count
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,15 +7,28 @@ class HashSet
   end
 
   def insert(key)
+    return false if include?(key)
+    self.count += 1
+    resize! if count >= store.length
+    index = key.hash % store.length
+    store[index] << key
   end
 
   def include?(key)
+    index = key.hash % store.length
+    store[index].include?(key)
   end
 
   def remove(key)
+    return unless include?(key)
+    self.count -= 1
+    index = key.hash % store.length
+    store[index].delete(key)
   end
 
   private
+
+  attr_reader :store
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
@@ -26,5 +39,16 @@ class HashSet
   end
 
   def resize!
+    count.times do
+      store << Array.new
+    end
+
+    store.each do |bucket|
+      bucket.length.times do
+        element = bucket.shift
+        index = element.hash % store.length
+        store[index] << element
+      end
+    end
   end
 end
