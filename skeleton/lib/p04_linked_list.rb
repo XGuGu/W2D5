c8be141e8cd3fcc7244ff18b9563c89728eaa742
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-03 14:59:52 -0400

    Finish phase 4

--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -15,11 +15,20 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+
+    self.prev.next = self.next
+    self.next.prev = self.prev
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new("head", "head")
+    @tail = Node.new("tail", "tail")
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,31 +37,79 @@ class LinkedList
   end
 
   def first
+    head.next
   end
 
   def last
+    tail.prev
   end
 
   def empty?
+    head.next == tail && tail.prev == head
   end
 
   def get(key)
+    node = head.next
+
+    until node == tail
+      return node.val if node.key == key
+      node = node.next
+    end
+
+    nil
   end
 
   def include?(key)
+    !get(key).nil?
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    tail.prev.next = node
+    node.prev = tail.prev
+    node.next = tail
+    tail.prev = node
   end
 
   def update(key, val)
+    node = head.next
+
+    until node == tail
+      if node.key == key
+        node.val = val
+      end
+
+      node = node.next
+    end
   end
 
   def remove(key)
+    node = head.next
+
+    until node == tail
+      if node.key == key
+        node.remove
+      end
+
+      node = node.next
+    end
   end
 
   def each
+    node = head.next
+
+    until node == tail
+      yield(node)
+
+      node = node.next
+    end
+
+    self
   end
+
+  private
+
+  attr_reader :head, :tail
 
   # uncomment when you have `each` working and `Enumerable` included
   # def to_s
